66d8ea86168f340c83f0764ebee4c3780ea3792d
nisino25 <nisino25@gmail.com>
2021-03-26 19:16:27 -0700

    first commit

---
+++ src/const/ollShuffle.js
@@ -0,0 +1,70 @@
+export const  rolesList = {
+
+  0: [`L' U2 L U L' U' L U L' U L`, `F' L2 B2 D2 B' D2 B' L2 F2 U2 F'`, `B' U' B U' B' U B U' B' U2 B`],
+  1: [`B R' D2 R B' U B R' D2 R B'`, `R U B' R2 D' F R F' R2 D B R2`, `B U B' U B U F' U B' U' F`],
+  2: [`B U B' U2 F U' B U F' U B'`, `F U2 R2 F' U2 F U2 R2 B U2 B' F'`, `B L U L2 B2 D' B' D B' L U' B'`],
+  3: [`F' R L F R2 F' L' F R2 F' R' F`, `R' B F R B2 R' F' R B2 R' B' R`, `L F R U' R' F' U F U F' L'`],
+  4: [`B2 U2 B R' F' B' U F U2 B R B`, `R' U F U' F' U' R U' F U' F'`, `B' F R2 F R2 F2 U F U' B U2 F'`],
+  5: [`F L2 F U2 F' D' F U2 F' D L2 F'`, `B U B2 U' B2 U' B F R2 B2 R2 F'`, `L' B2 R D L D' R' D L' D' B2 L`],
+  6: [`F B' U F2 D' F' D F' U' R2 F' B`, `R L B' L B R B' L' B R2 L'`, `R' L F2 D' L D L' F2 R L2 U2 L`],
+
+
+  7: [`L F U' F' U L' B2 R B R' U B`, `B L U' F U' F2 L2 B L' F L2 B2`, `R' U2 B L2 F' L2 B' U2 L F R L'`],
+  8: [`F R F' U2 B U2 B' U2 F R' F'`, `B' U' R' U B F' U' F U B' R B`, `B' U' R' U2 R B' D' F R' F' D B2`],
+  9: [`L F2 R' F' R L2 D2 R2 B R2 D2 L`, `L' U' L U' F R U' B U2 B' R' F'`, `B U L U' L' B' R B U B' U' R'`],
+  10: [`L' B2 L2 U2 L' B L U2 L2 B2 L`, `R B2 R2 U2 R B R' U2 R2 B2 R'`, `F' U' L' U L F L F U F' U' L'`],
+  11: [`F' B L B L B' L' F B2 R B R'`, `R' L D F D' R L B L B' U' L`, `F' B D L D' F B R B R' U' B`],
+  12: [`B L U' F U' F' L' B' R' U2 R`, `F U2 F' L' U' L' B L B2 U' B L`, `R U R' F' L' B' U' B U' L U F`],
+  13: [`B L F U2 F' U L' B' R' U2 R`, `L' B L F B2 R' B' R B R F' B`, `R B' R' B U R L' D B' D' R' L`],
+  14: [`F' B2 D L' D' F B2 R' F' U F R`, `F2 L' R U2 B' L2 R2 F' D2 L' R F2`, `R2 B F' U2 L' B2 F2 R U2 B F' L2`],
+
+  
+  15: [`L' U' B' U B2 L2 B' U2 B' U2 B L'`, `B' U' R' U R2 B L' B' R' B L`, `L F U F' U' L2 U' L U' L' U2 L`],
+  16: [`R U R' B2 R D B U' B' D' R' B2`, `R B U B' U R2 U' R2 U' R2 U2 R`, `R U B U' B2 R' F R B R' F'`],
+  17: [`B' U' B R D' B2 U B' U' B2 D R'`, `L F R U' R' U R F' L' F R' F'`, `B' U' B2 L U L' B2 U B2 U2 B'`],
+  18: [`L' U2 L U F R B U B' R' F'`, `F' U2 F U F' U F2 U R U' R' F'`, `F' L' U L F2 D B' R' B D' F'`],
+  19: [`R2 U2 R' U' B U B' U R U R2`, `L2 B2 R D' B R' B' R D R' B2 L2`, `R U2 L' U' L B L' U L B' U2 R'`],
+  20: [`B U L U' L B R2 B' L2 B R2 B2`, `R U B' U' B' R' B U B2 U' B'`, `F' L U L U L' U' L2 F L U' L'`],
+  21: [`B L' B L B2 U B L U L' B'`,`L' R2 B L' B D B' D' B' L2 R2`,`B' F2 R B' R D R' D' R' B2 F2`],
+  22: [`L' U' B' R' F' U F U R B L`,`R' L F' L F R L2 U' B' U' B`,`R B2 D2 F L' F' D' B D' R' B`],
+  23: [`B U L U' R2 L B2 F2 R2 L2 B F2`, `R2 L2 U2 D2 R2 L U' B' U B L D2`, `F2 L2 R2 B' D R D' R' B L2 R2 F2`],
+  24: [`R L' U R' F' U' L' U' L F L`, `F R' F' R U2 R2 D L' B2 L D' R2`, `B U B' U' B' U' R U B U' R' `],
+  25: [`B' U2 B U B' R B' R' B U B`, `B D L' D' L2 U L U' L2 B2 U2 B`, `F R' F R F' U' F' U L' U2 L`],
+  26: [`R D L2 D L2 D' B' D' B R2 U2 R`, `F2 B L' F L' F' L D' L D F2 B'`, `B L2 F2 D F L D' B' L2 F U' L'`],
+  27: [`B' U' B' R B U2 R2 U' R2 U' R' B`, `F' L F2 R2 D R D2 F D L' F2 R`, `L' U L U' L2 B L2 U L' U' B' L`],
+  28: [`L' B L' B' L2 B' U F U' B U2 F'`, `L F' L F L2 F U' B' U F' U2 B`, `L F U' F' U F R U R' U F' U' L'`],
+  29: [`F' L2 U' L' U B L2 B' F L U L'`, `B L U2 L2 B2 D' B' D B' L U2 B'`, `F' L' F' D F2 D2 B' L U B L' D`],
+
+
+  29: [``, `'`, ``],
+  30: [``, ``, ``],
+  31: [``, ``, ``],
+  32: [``, ``, ``],
+  33: [``, ``, ``],
+  34: [``, ``, ``],
+  35: [``, ``, ``],
+  36: [``, ``, ``],
+  37: [``, ``, ``],
+  38: [``, ``, ``],
+  39: [``, ``, ``],
+  40: [``, ``, ``],
+  41: [``, ``, ``],
+  42: [``, ``, ``],
+  43: [``, ``, ``],
+  44: [``, ``, ``],
+  45: [``, ``, ``],
+  46: [``, ``, ``],
+  47: [``, ``, ``],
+  48: [``, ``, ``],
+  49: [``, ``, ``],
+  50: [``, ``, ``],
+  51: [``, ``, ``],
+  52: [``, ``, ``],
+  53: [``, ``, ``],
+  54: [``, ``, ``],
+}
+// rolesList.Werewolf.push({name: '',  imgLink: ''});
+
+
+rolesList.Village.push({name: 'Villager', avialblity: true, amountOfAbility: 100,  imgLink: 'https://static.wikia.nocookie.net/werewolf-online/images/5/58/Villager.png/revision/latest/scale-to-width-down/620?cb=20190629192110'});
+rolesList.Village.push({no: 'Villager', group: 'line', avialblity: false, amountOfAbility: 100,  imgLink: 'https://static.wikia.nocookie.net/werewolf-online/images/5/58/Villager.png/revision/latest/scale-to-width-down/620?cb=20190629192110'});
